7d1b81046435e5e0ca20d40ecf824e5ac3453a92
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-08-26 11:33:42 -0700

    add editor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import React from 'react';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import axios from 'axios'
+import axios from 'axios';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-monokai';
+
 export default function Home() {
   const [code, setCode] = useState('');
   const [result, setResult] = useState('');
@@ -18,10 +24,15 @@ export default function Home() {
     <div className="min-h-screen bg-black text-white">
       <div className="container mx-auto py-10 px-5">
         <h1 className="text-4xl font-bold mb-5 text-teal-500">Smart Contract Auditor</h1>
-        <textarea 
-          className="w-full h-64 p-2 mb-5 bg-gray-800 text-white rounded border border-gray-700 font-mono" 
-          value={code} 
-          onChange={(e) => setCode(e.target.value)} 
+        <AceEditor
+          mode="solidity"
+          theme="github"
+          onChange={setCode}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          value={code}
+          width='100%'
+          className="w-100 h-64 p-2 mb-5 bg-gray-800 text-white rounded border border-gray-700 font-mono" 
         />
         <button 
           className="py-2 px-4 bg-teal-500 text-white rounded hover:bg-teal-600" 
