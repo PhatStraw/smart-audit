9630609a11c51c4f6e2e0c9906f744bbe9cb935e
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-08-26 11:59:57 -0700

    restyle

--- pages/index.js
+++ pages/index.js
@@ -10,38 +10,40 @@ import 'ace-builds/src-noconflict/theme-monokai';
 export default function Home() {
   const [code, setCode] = useState('');
   const [result, setResult] = useState('');
-  const [loading, setLoading] = useState(false); // Add loading state
+  const [loading, setLoading] = useState(false);
 
   const handleAudit = async () => {
-    setLoading(true); // Set loading to true when the request starts
+    setLoading(true);
     const response = await axios.post('/api/hello', { code });
-    console.log(response);
     setResult(response.data.response);
-    setLoading(false); // Set loading to false when the request finishes
+    setLoading(false);
   };
 
   return (
-    <div className="min-h-screen bg-black text-white">
+    <div className="min-h-screen bg-gray-100 text-gray-800">
       <div className="container mx-auto py-10 px-5">
-        <h1 className="text-4xl font-bold mb-5 text-teal-500">Smart Contract Auditor</h1>
-        <AceEditor
-          mode="solidity"
-          theme="github"
-          onChange={setCode}
-          name="UNIQUE_ID_OF_DIV"
-          editorProps={{ $blockScrolling: true }}
-          value={code}
-          width='100%'
-          className="w-100 h-64 p-2 mb-5 bg-gray-800 text-white rounded border border-gray-700 font-mono" 
-        />
-        <button 
-          className="py-2 px-4 bg-teal-500 text-white rounded hover:bg-teal-600" 
-          onClick={handleAudit}
-          disabled={loading} // Disable the button when loading
-        >
-          {loading ? 'Loading...' : 'Audit'} 
-        </button>
-        <div className="mt-5">
+        <h1 className="text-4xl font-bold mb-5 text-blue-600">Smart Contract Auditor</h1>
+        <div className="shadow-lg bg-white p-5 rounded-lg">
+          <AceEditor
+            mode="javascript"
+            theme="monokai"
+            onChange={setCode}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            value={code}
+            width='100%'
+            className="w-full h-64 mb-5 rounded border border-gray-300"
+            style={{ fontSize: '16px' }}
+          />
+          <button 
+            className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600" 
+            onClick={handleAudit}
+            disabled={loading}
+          >
+            {loading ? 'Loading...' : 'Audit'} 
+          </button>
+        </div>
+        <div className="mt-5 shadow-lg bg-white p-5 rounded-lg">
           <ReactMarkdown className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl 2xl:prose-2xl">
             {result}
           </ReactMarkdown>  
