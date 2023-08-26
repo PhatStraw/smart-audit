05f75f56cbf46bf8792dc0db62761a9e4698ac53
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-08-25 17:50:51 -0700

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+require('dotenv').config()
+import { OpenAI } from 'langchain/llms/openai';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const openai = new OpenAI({
+  openAIApiKey: process.env.NEXT_PUBLIC_CHAT_GPT,
+});
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { code } = req.body;
+    console.log(11)
+    try {
+      const response = await openai.predict(`As a senior blockchain developer, audit the following smart contract: ${code}. Please provide the following sections in your response,a paragraph long "summary" of the contract, a list of "flaws", a list of "fixes", all in markdown format with titles(all caps), bullets, and double spacing between sections.`);
+      console.log(response)
+      // const auditResult = response.auditResult;
+
+      res.status(200).json({ response });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
 }
