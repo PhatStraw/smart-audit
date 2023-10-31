2b9ca4471331da3c6b02664724c7c1ee3f67b40c
PhatStraw <kevindsimsjr@gmail.com>
2023-10-30 23:23:46 -0700

    styles

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
-import { OpenAI } from 'langchain/llms/openai';
+require("dotenv").config();
+import { OpenAI } from "langchain/llms/openai";
 
 const openai = new OpenAI({
   openAIApiKey: process.env.NEXT_PUBLIC_CHAT_GPT,
 });
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { code } = req.body;
-    console.log(11)
+    console.log(11);
     try {
       const response = await openai.predict(
         `As a senior blockchain developer, use past information on blockchain exploits and solidity best practices and audit the following smart contract: 
@@ -19,12 +19,13 @@ export default async function handler(req, res) {
       
       
 
-      Use your knowlege from past exploits and best solidity practices to provide the following sections in your response,a paragraph long "summary" of the contract, a list of "flaws", a list of "fixes", all in markdown format with titles(all caps and bold), bullets, and double spacing between sections.`);
+      Use your knowlege from past exploits and best solidity practices to provide the following sections in your response,a paragraph long "summary" of the contract, a list of "flaws", a list of "fixes", all in markdown format with titles(all caps and bold), bullets, and double spacing between sections.`
+      );
       res.status(200).json({ response });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed' });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import axios from 'axios';
-import AceEditor from 'react-ace';
+import React from "react";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import axios from "axios";
+import AceEditor from "react-ace";
 
-import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/theme-monokai';
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
 
 export default function Home() {
-  const [code, setCode] = useState('');
-  const [result, setResult] = useState('');
+  const [code, setCode] = useState("");
+  const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleAudit = async () => {
     setLoading(true);
-    const response = await axios.post('/api/hello', { code });
+    const response = await axios.post("/api/hello", { code });
     setResult(response.data.response);
     setLoading(false);
   };
@@ -22,7 +22,9 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
       <div className="container mx-auto py-10 px-5">
-        <h1 className="text-4xl font-bold mb-5 text-blue-600">Smart Contract Auditor</h1>
+        <h1 className="text-4xl font-bold mb-5 text-blue-600">
+          Smart Contract Auditor
+        </h1>
         <div className="shadow-lg bg-white p-5 rounded-lg">
           <AceEditor
             mode="javascript"
@@ -31,16 +33,16 @@ export default function Home() {
             name="UNIQUE_ID_OF_DIV"
             editorProps={{ $blockScrolling: true }}
             value={code}
-            width='100%'
+            width="100%"
             className="w-full h-64 mb-5 rounded border border-gray-300"
-            style={{ fontSize: '16px' }}
+            style={{ fontSize: "16px" }}
           />
           <button
             className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
             onClick={handleAudit}
             disabled={loading}
           >
-            {loading ? <div className="loader"></div> : 'Audit'}
+            {loading ? <div className="loader"></div> : "Audit"}
           </button>
         </div>
         <div className="mt-5 shadow-lg bg-white p-5 rounded-lg">
