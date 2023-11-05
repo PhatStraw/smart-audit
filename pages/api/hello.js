1b1f338a8599f27ba6ccf7a4055556b41ba63965
PhatStraw <kevindsimsjr@gmail.com>
2023-11-05 00:09:19 -0700

    updatedprompt

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -14,9 +14,7 @@ export default async function handler(req, res) {
         `you are a senior blockchain developer, interviewing for a high paying position. given this smart contract how could I be exploited? 
         
         smart contract: ${code}
-      `,{
-        temperature: 0
-      }
+      `
       );
       res.status(200).json({ response });
     } catch (error) {
